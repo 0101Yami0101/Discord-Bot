7a020f01372f07429fc9cc11efd086ec7768d9c9
Yami <97562641+0101Yami0101@users.noreply.github.com>
2024-11-25 17:19:35 +0530

    Update bot.py
    Minor Update : Remove Comments

--- bot.py
+++ bot.py
@@ -24,42 +24,42 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def load_all_cogs():
     # List of all cogs to load
     cogs = [
-        # "basics.channel",
-        # "basics.create_events",
-        # "basics.create_posts",
-        # "basics.create_polls",
-        # "basics.create_announcements",
-        # "basics.embeds",
-        # "basics.reminder",
-        # "basics.reaction_roles",
-        # "basics.roles",
-        # 'basics.system',
-        # "basics.levelling_system",
-        # "basics.tickets",
-        # "basics.welcome",
-        # "Moderation.auto_mod_init",
-        # "Moderation.profanity_check",
-        # "Moderation.spam_detect",
-        # "Moderation.caps_lock",
-        # "Moderation.links_and_invites",
-        # "Moderation.temp_ban",
-        # "Moderation.permanent_ban",
-        # "Moderation.manual.blacklist",
-        # "Moderation.manual.create_channels",
-        # "Moderation.manual.mute",
-        # "Moderation.manual.kick",
-        # "Moderation.manual.custom_ban",
-        # "Moderation.manual.slowmode",
-        # "Moderation.manual.invites_tracker",
-        # "Moderation.manual.whitelist_links",
-        # "games.raffle",
-        # "games.quiz",
-        # "games.hangman",
-        # "games.tictactoe",
-        # "special.image_filter",
-        # "special.verification",
-        # "special.translate",
-        # "special.chat",
+        "basics.channel",
+        "basics.create_events",
+        "basics.create_posts",
+        "basics.create_polls",
+        "basics.create_announcements",
+        "basics.embeds",
+        "basics.reminder",
+        "basics.reaction_roles",
+        "basics.roles",
+        'basics.system',
+        "basics.levelling_system",
+        "basics.tickets",
+        "basics.welcome",
+        "Moderation.auto_mod_init",
+        "Moderation.profanity_check",
+        "Moderation.spam_detect",
+        "Moderation.caps_lock",
+        "Moderation.links_and_invites",
+        "Moderation.temp_ban",
+        "Moderation.permanent_ban",
+        "Moderation.manual.blacklist",
+        "Moderation.manual.create_channels",
+        "Moderation.manual.mute",
+        "Moderation.manual.kick",
+        "Moderation.manual.custom_ban",
+        "Moderation.manual.slowmode",
+        "Moderation.manual.invites_tracker",
+        "Moderation.manual.whitelist_links",
+        "games.raffle",
+        "games.quiz",
+        "games.hangman",
+        "games.tictactoe",
+        "special.image_filter",
+        "special.verification",
+        "special.translate",
+        "special.chat",
         "special.music_player",
     ]
 
@@ -71,10 +71,10 @@ async def load_all_cogs():
 @bot.event
 async def on_ready():
     #Register groups
-    bot.tree.add_command(create_group) #todo- CREATE CLASS FOR GROUPS
-    bot.tree.add_command(role_group) #todo- CREATE CLASS FOR GROUPS
-    bot.tree.add_command(channel_group) #todo- CREATE CLASS FOR GROUP
-    bot.tree.add_command(raffle_group) #todo- CREATE CLASS FOR GROUP
+    bot.tree.add_command(create_group)
+    bot.tree.add_command(role_group)
+    bot.tree.add_command(channel_group) 
+    bot.tree.add_command(raffle_group) 
         
     await load_all_cogs()
     print(f'Logged in as {bot.user}')
